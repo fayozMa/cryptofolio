c7bf0efeb8cd7dfe7bf8258cc3ff20b9e63e262e
Fayyozbek <usmonovbahriddin108@gmail.com>
2024-12-15 21:13:42 +0500

    [FIX] tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,18 +10,18 @@ export default {
       container: {
         center: true,
         screens: {
-          lg: '1256px',
+          lg: "1256px",
         },
-        padding:"1.5rem"
+        padding: "1.5rem",
       },
       colors: {
-        lightBlue: '#87CEEB',
-        darkGray: '#14161A',
+        lightBlue: "#87CEEB",
+        darkGray: "#14161A",
       },
       boxShadow: {
-        'custom-1': '0px 4px 5px 0px #00000024',
+        "custom-1": "0px 4px 5px 0px #00000024",
       },
     },
   },
-  plugins: [require('@tailwindcss/typography'),require('daisyui')],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
